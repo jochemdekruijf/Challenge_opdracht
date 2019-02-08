e084d0f13bbf8a007e8a4daafd5533624fbc608a
Jochem de Kruijf <jochemdekruijfff@outlook.com>
2019-02-08 09:41:37 +0100

    maak eigen pizza, nog niet af

--- js/pizzaCalc.js
+++ js/pizzaCalc.js
@@ -1,39 +1,31 @@
 
 
-var dataOrder = [];
+var dataOrder = [[]];
 var dataOrderT = ["Quattro Stagion","Romana","Frute di Mare","Tonno (e cipolla)"];
+var slices = 0;
+var price = 7;
+var globalPrice = 0;
+var topping_prices = [0.75,0.75,0.50,1,0.50]; //cheese, mushroom, salami, pineapple, ham
+var added_toppings = [];
 
-   var pizzaArrayCV = [
-          "Margherita",
-          "Marinara",
-          "Quattro Formaggi",
-          "Verdure / Vegetariana",
-          "Carciofi",
-          "Funghi"
-          ];
+var p_saus = 1;
+var pizzaArrayCV = [ "Margherita","Marinara","Quattro Formaggi","Verdure / Vegetariana","Carciofi", "Funghi"];
+var pizzaArrayMeat = ["Quattro Stagioni","Calzone","Capricciosa","Prosciutto","Diavola / Piccante" ];          
+var pizzaArrayFish = ["Napoli","Romana","Tonno (e cipolla)",  "Frute di Mare"];
+var base = 0;
+var discount = false;
+var ordered = false;
+var emptied = false;
 
 
-  var pizzaArrayMeat = [
-         "Quattro Stagioni",
-          "Calzone",
-          "Capricciosa",
-          "Prosciutto",
-          "Diavola / Piccante"
-          ];    
-
-  var pizzaArrayFish = [
-         "Napoli",
-          "Romana",
-          "Tonno (e cipolla)",
-          "Frute di Mare"
-          ];
+                  
  var descript_CV = [
-         "tomaat, mozzarella, basilicum",
-          "tomaat, knoflook, olie en oregano (pas als de pizza uit de oven komt)",
-          "gorgonzola, mozzarella, pecorino en taleggio. In Nederland wordt de pecorino en taleggio vaak vervangen door Parmezaanse kaas en gewone Hollandse (Goudse) kaas",
-          "tomaat, mozzarella, paprika en (gegrilde) courgette en aubergine. Soms ook broccoli of spinazie.",
-          "tomaat, mozzarella en artisjok",
-          "tomaat, mozzarella en (verse) champignons"
+     "tomaat, mozzarella, basilicum",
+      "tomaat, knoflook, olie en oregano (pas als de pizza uit de oven komt)",
+      "gorgonzola, mozzarella, pecorino en taleggio. In Nederland wordt de pecorino en taleggio vaak vervangen door Parmezaanse kaas en gewone Hollandse (Goudse) kaas",
+      "tomaat, mozzarella, paprika en (gegrilde) courgette en aubergine. Soms ook broccoli of spinazie.",
+      "tomaat, mozzarella en artisjok",
+      "tomaat, mozzarella en (verse) champignons"
       ];
  var descript_M = [
       "tomaat, mozzarella, ham, champignons, olijven, artisjokken (soms doperwten in plaats van artisjok)",
@@ -50,13 +42,9 @@ var dataOrderT = ["Quattro Stagion","Romana","Frute di Mare","Tonno (e cipolla)"
       "tomaat, mozzarella, vongole, garnalen, mosselen, inktvis, langoustine en peterselie (na het bakken van de pizza)"
       ];     
        
-          function mouseOver (){
 
 
-          }
-
-
-
+     
         function showPizzaCV () {
 
       for (var i = 0; i < pizzaArrayCV.length; i++) {
@@ -73,15 +61,6 @@ var dataOrderT = ["Quattro Stagion","Romana","Frute di Mare","Tonno (e cipolla)"
             img.setAttribute("width", "17");
             img.setAttribute("alt", "plus symbol");
             document.getElementById("CV"+i).appendChild(img);
-
-            // var imgI = document.createElement("img");
-
-            // imgI.setAttribute("src", "images/info.png");
-            // imgI.setAttribute("id", "imgI"+i);
-            // imgI.setAttribute("height", "15");
-            // imgI.setAttribute("width", "15");
-            // imgI.setAttribute("alt", " info symbol");
-            // document.getElementById("CV"+i).appendChild(imgI);
             
             var divI = document.createElement("div");
 
@@ -120,16 +99,6 @@ var dataOrderT = ["Quattro Stagion","Romana","Frute di Mare","Tonno (e cipolla)"
             img.setAttribute("alt", "plus symbol");
             document.getElementById("M"+i).appendChild(img);
 
-            //var imgI = document.createElement("img");
-
-            // imgI.setAttribute("src", "images/info.png");
-            // imgI.setAttribute("id", "imgI"+i);
-            // imgI.setAttribute("height", "15");
-            // imgI.setAttribute("width", "15");
-            // imgI.setAttribute("alt", " info symbol");
-            // document.getElementById("M"+i).appendChild(imgI);
-
-
              var divI = document.createElement("div");
 
             var text_info = document.createTextNode( descript_M[i]); 
@@ -166,15 +135,6 @@ var dataOrderT = ["Quattro Stagion","Romana","Frute di Mare","Tonno (e cipolla)"
           img.setAttribute("alt", "plus symbol");
           document.getElementById("F"+i).appendChild(img); 
 
-          // var imgI = document.createElement("img");
-
-          // imgI.setAttribute("src", "images/info.png");
-          // imgI.setAttribute("id", "imgI"+i);
-          // imgI.setAttribute("height", "15");
-          // imgI.setAttribute("width", "15");
-          // imgI.setAttribute("alt", " info symbol");
-          // document.getElementById("F"+i).appendChild(imgI);
-
 
           var divI = document.createElement("div");
 
@@ -201,13 +161,13 @@ var dataOrderT = ["Quattro Stagion","Romana","Frute di Mare","Tonno (e cipolla)"
 
            //  }else
 
-           for (var i = 0; i < dataOrderT.length; i++) {
+           for (var i = 0; i < dataOrder.length; i++) {
 
           var listItemOrder = document.createElement("LI");        // Create a <li> element
-          var t = document.createTextNode(dataOrderT[i]);     // Create a text node
-           listItemOrder.setAttribute("id", "Order"+i);
+          var t = document.createTextNode(dataOrder[i]);     // Create a text node
+          listItemOrder.setAttribute("id", "Order"+i);
           listItemOrder.appendChild(t);                            // Append the text to <li>   
-          document.getElementById("orderList").appendChild(listItemOrder);
+          document.getElementById("orderlist").appendChild(listItemOrder);
 
           var imgMin = document.createElement("img");
 
@@ -215,13 +175,138 @@ var dataOrderT = ["Quattro Stagion","Romana","Frute di Mare","Tonno (e cipolla)"
           imgMin.setAttribute("height", "17");
           imgMin.setAttribute("width", "17");
           imgMin.setAttribute("alt", "minus symbol");
-          document.getElementById("Order"+i).appendChild(imgMin); 
-           
-
+          document.getElementById("Order"+i).appendChild(imgMin);            
+           }
           }
+
+           function addCheese (){
+
+               price = price + topping_prices[0];
+               added_toppings.push("Kaas");
+               displayPrice();
+               displayToppings();
+               base++;
+         }
+
+           function addMushroom (){
+
+               price = price + topping_prices[1];
+               added_toppings.push("Champignon");
+               displayPrice();
+               displayToppings();
+               base++;  
+             
+           }
+
+            function addSalami(){
+
+               price = price + topping_prices[2];
+               added_toppings.push("Salami");
+               displayPrice();
+               displayToppings();
+               base++;  
+             
+           }
+
+             function addPineapple (){
+
+               price = price + topping_prices[3];
+               added_toppings.push("Ananas");
+               displayPrice();
+               displayToppings();
+               base++;  
+             
+           }
+             function addHam(){
+
+               price = price + topping_prices[4];
+               added_toppings.push("Ham");
+               displayPrice();
+               displayToppings();
+               base++;  
+             
+           }
+
+           function addSaus (){
+               document.getElementById('bodem').style.display = "none";
+               document.getElementById('bodemS').style.display = "block";    
+               price = price + p_saus;
+               displayPrice();
+
+             
+           }
+
+
+         function displayPrice(){
+          
+           var number = price;
+           var gPrice = globalPrice;
+           document.getElementById("price_display").innerHTML = "€" + number;
+           document.getElementById("orderlist").innerHTML = "€" + gPrice;
+
+         }
+
+         function displayToppings(){
+          // if(emptied === true){
+          // price = price;
+          // base = 0;
+          // added_toppings = [];
+          // }
+         
+          for (var i = base ; i < added_toppings.length; i++) {
+
+          var Li = document.createElement("LI");        // Create a <li> element
+
+          var added_top = document.createTextNode(added_toppings[i]);     // Create a text node
+          //Li.setAttribute("id", "F"+i);
+          Li.appendChild(added_top);                            // Append the text to <li>   
+          document.getElementById("toppings").appendChild(Li);
+          }
+
+       }
+
+        function empty() {
+          
+         price = 7;
+         base = 0;
+         added_toppings = [];
+         emptied = true;
+         displayPrice();
+         displayToppings();
+         
+     
+      }
+
+      function order () {
+       ordered = true;
+       globalPrice = globalPrice + price; 
+       dataOrder.push(added_toppings);
+       displayPrice();
+       empty();
+       displayToppings();
+      }
+
+      function  payBank() {
+        
+   
+      if (discount === false && ordered === true) {
+        discount = true;
+        globalPrice = globalPrice - 2;
+        document.getElementById('cash').style.display = 'none';
+        displayPrice();
+
+        }
+        document.getElementById('cash').style.display = 'none';
+        displayPrice();
+       
+      }
+
+       function  payCash() {
+        
+      document.getElementById("bank").style.display = "none";
+      displayPrice();
+      }
+        
  
-
-          }
-      
 
 
