70bbc762f7ed8978540cf4b959e1139c35cb0523
Jochem de Kruijf <jochemdekruijfff@outlook.com>
2019-02-13 13:05:40 +0100

    alert messages and backlog has been added

--- js/pizzaCalc.js
+++ js/pizzaCalc.js
@@ -4,7 +4,6 @@ var globalPrice = 0;
 var topping_prices = [1, 0.75, 0.75, 0.50, 1, 0.50]; //cheese,mozzarella mushroom, salami, pineapple, ham
 var toppingBoolean = [false,false,false,false,false,false];
 var added_toppings = [];
-var t_Sauce = 1;
 var base = 0;
 var pizzaCount = 0;
 var selectIngredient = false;
@@ -14,6 +13,8 @@ var ordered = false;
 var del_fee = false;
 var sauceBoolean = false;
 var size = false;
+var ingredientCheck = false;
+
 
 
 function showOrder() {
@@ -25,8 +26,77 @@ function showOrder() {
   listItemOrder.setAttribute("id", "Order" + i);
   listItemOrder.appendChild(t); // Append the text to <li>   
   document.getElementById("orderlist").appendChild(listItemOrder);
+  console.log('order updated');
  }
 }
+
+function addFromMenu(pizza){
+ 
+     if(pizza === "margerita"){
+
+       dataOrder.push("margerita");
+       globalPrice = globalPrice + 10.50;
+       ordered = true;
+       alertNotification('menuAlert');
+       console.log('magherita from menu added to order');
+       
+     }
+
+      if(pizza === "hawaii"){
+
+       dataOrder.push("pizza Hawaii");
+       globalPrice = globalPrice + 11.50;
+       ordered = true;
+       alertNotification('menuAlert');
+       console.log('Hawaii from menu added to order');
+
+     }
+
+      if(pizza === "salami"){
+
+       dataOrder.push("salami");
+       globalPrice = globalPrice + 10.50;
+       ordered = true;
+       alertNotification('menuAlert');
+       console.log('Salami from menu added to order');
+
+     }
+
+      if(pizza === "funghi"){
+
+       dataOrder.push("funghi");
+       globalPrice = globalPrice + 13.50;
+       ordered = true;
+       alertNotification('menuAlert');
+       console.log('Funghi from menu added to order');
+     }
+
+      if(pizza === "quattro-formaggi"){
+ 
+       dataOrder.push("quattro-formaggi");
+       globalPrice = globalPrice + 14.50;
+       ordered = true;
+       alertNotification('menuAlert');
+       console.log('quattro formaggi from menu added to order');
+     }
+
+      if(pizza === "vegetarian"){
+
+       dataOrder.push("vegetarian");
+       globalPrice = globalPrice + 10;
+       ordered = true;
+       alertNotification('menuAlert');
+       console.log('Vegetarian from menu added to order');
+
+     }
+     displayPrice();
+     showOrder();
+
+}
+
+
+
+
 
 function selectedSize(selected) {
 
@@ -38,6 +108,7 @@ function selectedSize(selected) {
        document.getElementById('xlargeSizeB').style.display = 'none';
        selectSizeBoolean = true;
        size = true;
+       console.log('Selected size: normal  added to  added_toppings');
      }
 
       if(selected === "large" && size === false){
@@ -48,6 +119,7 @@ function selectedSize(selected) {
         price = price + 3;
         selectSizeBoolean = true;
         size = true;
+        console.log('Selected size: large  added to  added_toppings');
      }
 
       if(selected === "xlarge" && size === false){
@@ -58,77 +130,16 @@ function selectedSize(selected) {
         price = price + 5;
         selectSizeBoolean = true;
         size = true;
+        console.log('Selected size: extra large  added to  added_toppings');
      }
 
       if( selectSizeBoolean === false){
 
         base++;
-
-      } 
-     
+        
+      }  
   displayToppings();
   displayPrice();
-}
-
-
- function addIngredient(ingredient){
-
-    if (ingredient === "cheese" && toppingBoolean[0] === false) {
-
-     price = price + topping_prices[0];
-     added_toppings.push("Kaas");
-     toppingBoolean[0] = true;
-     selectIngredient = true;
-    }
-
-    if (ingredient === "mozzarella" && toppingBoolean[1] === false) {
-
-     price = price + topping_prices[1];
-     added_toppings.push("mozzarella");
-     toppingBoolean[1] = true;
-     selectIngredient = true;
-    }
-
-    if (ingredient === "mushroom" && toppingBoolean[2] === false) {
-
-     price = price + topping_prices[2];
-     added_toppings.push("champignon");
-     toppingBoolean[2] = true;
-     selectIngredient = true;
-    }
-
-    if (ingredient === "salami" && toppingBoolean[3] === false) {
-
-     price = price + topping_prices[3];
-     added_toppings.push("salami");
-     toppingBoolean[3] = true;
-     selectIngredient = true;
-    }
-
-    if (ingredient === "pineapple" && toppingBoolean[4] === false) {
-
-     price = price + topping_prices[4];
-     added_toppings.push("ananas");
-     toppingBoolean[4] = true;
-     selectIngredient = true;
-    }
-
-    if (ingredient === "ham" && toppingBoolean[5] === false) {
-
-     price = price + topping_prices[5];
-     added_toppings.push("ham");
-     toppingBoolean[5] = true;
-     selectIngredient = true;
-
-     }
-
-     if (selectIngredient === false) {
-
-       base++;
-     }
-
- displayPrice();
- displayToppings();
 }
 
 function  Sauce(ingredient) {
@@ -137,12 +148,13 @@ function  Sauce(ingredient) {
      if(ingredient === "tomato" && sauceBoolean === false ){
 
        added_toppings.push("saus: tomatensaus");
-       price = price + t_Sauce;
+       price = price + 1.50;
        document.getElementById('bodem').style.display = "none";
        document.getElementById('bodemS').style.display = "block";
        document.getElementById('noSauce').style.display = "none";
        selectSauceBoolean = true;
        sauceBoolean = true;
+       console.log('Selected sauce: no sauce added to  added_toppings');
      }
 
       if(ingredient === "none" && sauceBoolean === false){
@@ -153,6 +165,7 @@ function  Sauce(ingredient) {
         document.getElementById('tomatoS').style.display = 'none';
         selectSauceBoolean = true;
         sauceBoolean = true;
+        console.log('Selected sauce: tomato sauce added to  added_toppings');
      }
 
       if( selectSauceBoolean === false){
@@ -165,66 +178,146 @@ function  Sauce(ingredient) {
   displayPrice();
 }
 
+ function addIngredient(ingredient){
 
-function displayPrice() {
+    if (ingredient === "cheese" && toppingBoolean[0] === false) {
 
- var number = price;
- var gPrice = globalPrice;
- document.getElementById("price_display").innerHTML = "€" + number;
- document.getElementById("orderlist").innerHTML = "€" + gPrice;
+     price = price + topping_prices[0];
+     added_toppings.push("Kaas");
+     toppingBoolean[0] = true;
+     selectIngredient = true;
+     ingredientCheck = true;
+      console.log('Selected ingredient: cheese added to  added_toppings');
+    }
 
-}
+    if (ingredient === "mozzarella" && toppingBoolean[1] === false) {
 
-function displayToppings() {
+     price = price + topping_prices[1];
+     added_toppings.push("mozzarella");
+     toppingBoolean[1] = true;
+     selectIngredient = true;
+     ingredientCheck = true;
+     console.log('Selected ingredient: mozzarella added to  added_toppings');
+    }
 
-  document.getElementById("toppings").innerHTML = "gekozen beleg: ";
+    if (ingredient === "mushroom" && toppingBoolean[2] === false) {
 
- for (var i = base; i < added_toppings.length; i++) {
+     price = price + topping_prices[2];
+     added_toppings.push("champignon");
+     toppingBoolean[2] = true;
+     selectIngredient = true;
+     ingredientCheck = true;
+     console.log('Selected ingredient: mushroom added to  added_toppings');
+    }
 
-  var Li = document.createElement("li"); // Create a <li> element
+    if (ingredient === "salami" && toppingBoolean[3] === false) {
 
-  var added_top = document.createTextNode(added_toppings[i] + ","); // Create a text node
-  //Li.setAttribute("id", "F"+i);
-  Li.setAttribute('style', 'display:inline-block;');
-  Li.appendChild(added_top); // Append the text to <li>   
-  document.getElementById("toppings").appendChild(Li);
- }
+     price = price + topping_prices[3];
+     added_toppings.push("salami");
+     toppingBoolean[3] = true;
+     selectIngredient = true;
+     ingredientCheck = true;
+     console.log('Selected ingredient: salami added to  added_toppings');
+    }
 
-}
+    if (ingredient === "pineapple" && toppingBoolean[4] === false) {
 
-function reset() {
- 
- price = 5;
- base = 0;
- toppingBoolean = [false,false,false,false,false,false];
- selectSizeBoolean = false;
- size = false;
- selectIngredient = false;
- sauceBoolean = false;
- selectSauceBoolean = false;
- added_toppings = [];
- document.getElementById('bodem').style.display = "block";
- document.getElementById('bodemS').style.display = "none";
- document.getElementById('tomatoS').style.display = "inline-block";
- document.getElementById('noSauce').style.display = "inline-block";
- document.getElementById('normalSizeB').style.display = 'inline-block';
- document.getElementById('largeSizeB').style.display = 'inline-block';
- document.getElementById('xlargeSizeB').style.display = 'inline-block';
+     price = price + topping_prices[4];
+     added_toppings.push("ananas");
+     toppingBoolean[4] = true;
+     selectIngredient = true;
+     ingredientCheck = true;
+     console.log('Selected ingredient: pineapple added to  added_toppings');
+    }
+
+    if (ingredient === "ham" && toppingBoolean[5] === false) {
+
+     price = price + topping_prices[5];
+     added_toppings.push("ham");
+     toppingBoolean[5] = true;
+     selectIngredient = true;
+     ingredientCheck = true;
+     console.log('Selected ingredient: ham added to  added_toppings');
+
+     }
+
+     if (selectIngredient === false) {
+
+       base++;
+     }
+
  displayPrice();
  displayToppings();
 }
 
+  function displayPrice() {
 
-function order() {
- ordered = true;
- size = false;
- globalPrice = globalPrice + price;
- dataOrder.push(added_toppings);
- reset();
- displayPrice();
- displayToppings();
- showOrder();
-}
+   var number = price;
+   var gPrice = globalPrice;
+   document.getElementById("price_display").innerHTML = "€" + number;
+   document.getElementById("orderlist").innerHTML = "€" + gPrice;
+   console.log('price updated');
+  }
+
+  function displayToppings() {
+
+    document.getElementById("toppings").innerHTML = "gekozen beleg: ";
+
+   for (var i = base; i < added_toppings.length; i++) {
+
+    var Li = document.createElement("li"); // Create a <li> element
+
+    var added_top = document.createTextNode(added_toppings[i] + ","); // Create a text node
+    //Li.setAttribute("id", "F"+i);
+    Li.setAttribute('style', 'display:inline-block;');
+    Li.appendChild(added_top); // Append the text to <li>   
+    document.getElementById("toppings").appendChild(Li);
+   }
+    console.log('toppings updated');
+  }
+
+  function reset() {
+   
+   price = 5;
+   base = 0;
+   toppingBoolean = [false,false,false,false,false,false];
+   selectSizeBoolean = false;
+   size = false;
+   selectIngredient = false;
+   sauceBoolean = false;
+   selectSauceBoolean = false;
+   ingredientCheck = false;
+   added_toppings = [];
+   document.getElementById('bodem').style.display = "block";
+   document.getElementById('bodemS').style.display = "none";
+   document.getElementById('tomatoS').style.display = "inline-block";
+   document.getElementById('noSauce').style.display = "inline-block";
+   document.getElementById('normalSizeB').style.display = 'inline-block';
+   document.getElementById('largeSizeB').style.display = 'inline-block';
+   document.getElementById('xlargeSizeB').style.display = 'inline-block';
+   displayPrice();
+   displayToppings();
+   console.log('create Own has been reset');
+  }
+
+
+  function order() {
+   if (ingredientCheck === true){
+   ordered = true;
+   globalPrice = globalPrice + price;
+   dataOrder.push(added_toppings);
+   reset();
+   displayPrice();
+   displayToppings();
+   showOrder();
+   alertNotification('added2order');
+   console.log('order added to dataOrder');
+  }else{
+    
+    alert('voeg beleg toe');
+  }
+
+ };
 
   function payBy(method) {
 
@@ -233,11 +326,13 @@ function order() {
          discount = true;
          globalPrice = globalPrice - 0.50;
          document.getElementById('cash').style.display = 'none';
+         console.log('pay by bank selected');
      }
 
     if (method ==='cash' && ordered === true) {
 
     document.getElementById("bank").style.display = "none";
+    console.log('pay by cash selected');
    }
 
   displayPrice();
@@ -251,14 +346,36 @@ function order() {
       del_fee = true;
       globalPrice = globalPrice + 4;
       document.getElementById('takeAway').style.display = 'none';
+      console.log(' selected method  is delivery');
     }
 
-    if (method ==='takeAway' && ordered === true) {
+    if (method ==='takeaway' && ordered === true) {
 
     document.getElementById("bank").style.display = "none";
-    
+    console.log(' selected method  is takeaway');
+
    }
     displayPrice();
     showOrder();
   }
 
+  function alertNotification(alertArgument) {
+
+    if (alertArgument === 'menuAlert') {
+
+      console.log('show notification: menu');
+      document.getElementById('fromMenu').style.display = "block";
+      setTimeout(function(){ document.getElementById('fromMenu').style.display = "none"; }, 900);
+   }
+       if (alertArgument === 'added2order') {
+
+      console.log('show notification: create own');
+      document.getElementById('added2order').style.display = "block";
+      setTimeout(function(){ document.getElementById('added2order').style.display = "none"; }, 900);
+   }
+
+}
+
+
+
+  //
